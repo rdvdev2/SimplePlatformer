483be0f32f1d2ddf107dff1665f9dafa8f94bb09
Roger Díaz Viñolas <rdvdev2@gmail.com>
2021-04-21 22:24:21 +0200

    [New] Load all textures on the game

--- src/scenes/resources/ResourceManager.h
+++ src/scenes/resources/ResourceManager.h
@@ -15,18 +15,42 @@ namespace SP::Scene::Resource {
         ResourceManager() {
             FontMoonhouse.Load();
 
+            TextureGoomba0.Load();
+            TextureGoomba1.Load();
+            TexturePlayer0.Load();
+            TexturePlayer1.Load();
+            TextureZombie0.Load();
+            TextureZombie1.Load();
+
             TextureMainMenuBackground.Load();
             TextureMainMenuPlayButton.Load();
 
-            TextureGameBackground.Load();
+            TextureGameBackgroundLayer0.Load();
+            TextureGameBackgroundLayer1.Load();
+            TextureGameBackgroundLayer2.Load();
+            TextureGround.Load();
+            TextureGroundSlope.Load();
+            TextureLightning.Load();
         }
 
         FONT(Moonhouse, moonhouse);
 
+        TEXTURE(Goomba0, characters/goomba_0);
+        TEXTURE(Goomba1, characters/goomba_1);
+        TEXTURE(Player0, characters/player_0);
+        TEXTURE(Player1, characters/player_1);
+        TEXTURE(Zombie0, characters/zombie_0);
+        TEXTURE(Zombie1, characters/zombie_1);
+
         TEXTURE(MainMenuBackground, main_menu/background);
         TEXTURE(MainMenuPlayButton, main_menu/play_button);
 
-        TEXTURE(GameBackground, scenery/background_3);
+        TEXTURE(GameBackgroundLayer0, scenery/background_layer_0);
+        TEXTURE(GameBackgroundLayer1, scenery/background_layer_1);
+        TEXTURE(GameBackgroundLayer2, scenery/background_layer_2);
+        TEXTURE(Ground, scenery/ground);
+        TEXTURE(GroundSlope, scenery/ground_slope);
+        TEXTURE(Lightning, scenery/lightning);
     };
 }
 
